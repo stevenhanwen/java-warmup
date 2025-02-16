63aed4f6796f07a7442bef97cc2a4f9e7d9020d2
Steven Kang <stevenhanwen.kang@outlook.com>
2025-02-16 14:40:45 -0500

    did more logic problems and moving on to string2 problems

--- src/logic1.java
+++ src/logic1.java
@@ -23,5 +23,83 @@ public class logic1 {
 
         return false; 
     }
-    
+
+    // You and your date are trying to get a table at a restaurant. 
+    // The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the 
+    // stylishness of your date's clothes. The result getting the table is encoded as an int value 
+    // with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). 
+    // With the exception that if either of you has style of 2 or less, then the result is 0 (no). 
+    // Otherwise the result is 1 (maybe).
+
+    public static int dateFashion(int you, int date) {
+        if (you > 7 && date > 2) {
+            return 2; 
+        }
+
+        if (date > 7 && you > 2) {
+            return 2; 
+        }
+
+        if (date < 3) {
+            return 0; 
+        }
+
+        if (you < 3) {
+            return 0; 
+        }
+
+        return 1; 
+    }
+
+
+// The squirrels in Palo Alto spend most of the day playing. 
+// In particular, they play if the temperature is between 60 and 90 (inclusive). 
+// Unless it is summer, then the upper limit is 100 instead of 90. 
+// Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+
+        if (isSummer) {
+            if (temp >= 60 && temp <= 100) {
+                return true; 
+            }
+        }
+
+        if (temp >= 60 && temp <= 90) {
+            return true; 
+        }
+
+        return false; 
+  
+    }   
+
+    // You are driving a little too fast, and a police officer stops you. 
+    // Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. 
+    // If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. 
+    // If speed is 81 or more, the result is 2. Unless it is your birthday
+    //  -- on that day, your speed can be 5 higher in all cases.
+
+    public static int caughtSpeeding (int speed, boolean isBirthday) {
+        if (isBirthday) {
+            if (speed <= 65) {
+                return 0; 
+            } else if (speed >= 66 && speed <= 85) {
+                return 1; 
+            } else if (speed >= 86) {
+                return 2; 
+            }
+        }
+
+        if (speed <= 60) {
+            return 0; 
+        } else if (speed >= 61 && speed <= 80) {
+            return 1; 
+        } else if (speed >= 81) {
+            return 2; 
+        }
+
+        return 0; 
+
+    }
+
 }
