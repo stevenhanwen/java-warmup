34ba54f65b6cfcbf4a5a313a166cbe08424967a4
Steven Kang <stevenhanwen.kang@outlook.com>
2025-02-17 20:38:22 -0500

    added a new problem to solve next time

--- src/string_medium.java
+++ src/string_medium.java
@@ -220,5 +220,13 @@ public class string_medium {
         return ""; 
     }
 
+    // Given a string and a non-empty word string, return a string made of each char just before 
+    // and just after every appearance of the word in the string. Ignore cases where there is 
+    // no char before or after the word, and a char may be included twice if it is between two words.
+
+    public static String wordEnds(String str, String word) {
+  
+    }
+
 
 }
